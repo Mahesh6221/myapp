0cae79c86e766e36faf54052432f7c291bac944b
Mahesh6221 <maheshpatil6221@gmail.com>
2025-04-29 14:28:56 +0530

    Configured production settings

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
